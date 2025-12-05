91f597fe212e5c866035aa0c0d58322f0747fa6b
Сергей Хатунцев <sergeykhatuntsev@MacBook-Air-Sergej-2.local>
2025-12-05 13:31:20 +0300

    Fix: bug of parallel importing is fixed

--- app/models_registry.py
+++ app/models_registry.py
@@ -1,38 +1,10 @@
 # доступные модели, форматы, параметры
-
-from dataclasses import dataclass
-from typing import Any, Dict, Mapping, List
+from typing import Any, Mapping, List
 import importlib
 
 from app.model_specs.catboost_classifier import get_catboost_classifier_info
 from app.model_specs.logistic_regression import get_logistic_regression_info
-
-
-@dataclass
-class ModelClassInfo:
-    """описание доступного класса модели для обучения
-    key: ключ, по которому обращаемся к модели
-    display_name: отображаемое в ui имя для читаемости
-    class_path: путь до класса
-    default_params: дефольные параметры создания модели, если не указаны пользователем
-    param_schema: описание настраиваемых гиперпараметров
-    """
-
-    key: str
-    display_name: str
-    class_path: str
-    default_params: Dict[str, Any]
-    param_schema: Dict[str, Dict[str, Any]]
-
-
-"""
-    Импортирует объект по строке вида 'module.submodule.ClassName'.
-
-    Пример:
-        cls = import_string("sklearn.linear_model.LogisticRegression")
-
-    :raises ImportError: если импорт не удался или строка пути некорректна.
-    """
+from app.schemas import ModelClassInfo
 
 
 def import_string(path: str) -> Any:
